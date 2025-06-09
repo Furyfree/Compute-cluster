4fbd8ca0806b3dbd8c81148f1a450d6748ecb260
AimOfStone <frank.rasmusen@gmail.com>
2025-06-09 12:56:14 +0200

    fix expired access token

--- backend/src/util/auth.py
+++ backend/src/util/auth.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 # Normally you'd keep this secret somewhere safe (like an env file)
 SECRET_KEY = "supersecretkey"
@@ -7,7 +7,8 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 15
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
-    expire = datetime.now() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
+    #expire = datetime.now() + (expires_delta or timedelta(minutes=15))
     to_encode = data.copy()
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
