4b6024b95a5eb749ffd57cba45edca759733bbe3
Patrick Byrne <pby.software@byrne.dk>
2025-06-21 16:09:58 +0200

    Add Guacamole router to FastAPI application

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from backend.src.api.routes import guacamole
 from src.api.routes import auth, server, proxmox, users
 
 app = FastAPI(
@@ -20,3 +21,4 @@ app.include_router(auth.router)
 app.include_router(server.router)
 app.include_router(proxmox.router)
 app.include_router(users.router)
+app.include_router(guacamole.router)
