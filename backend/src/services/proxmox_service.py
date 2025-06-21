e92dc34f7a5548536e3f6b72c9f0a851e05e0d56
Emil S <41843174+OneHandedWonder@users.noreply.github.com>
2025-06-21 15:27:53 +0200

    Update proxmox_service.py

--- backend/src/services/proxmox_service.py
+++ backend/src/services/proxmox_service.py
@@ -14,8 +14,7 @@ def sync_ldap_changes():
     """Sync LDAP users and groups to Proxmox"""
     return proxmox.access.domains("LDAP").sync.post(
         scope="both", 
-        full = 1,
-        remove_vanished = "entry;properties"
+        purge = True
         )
 
 def full_ldap_sync():
