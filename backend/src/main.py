170457d660ed7aa24e8ea99cf60f1cfc552e87b9
Patrick Byrne <pby.software@byrne.dk>
2025-06-22 17:03:48 +0200

    Reorder FastAPI router inclusion for better organization

--- backend/src/main.py
+++ backend/src/main.py
@@ -17,8 +17,8 @@ app.add_middleware(
 )
 
 app.include_router(auth.router)
-app.include_router(server.router)
-app.include_router(proxmox.router)
 app.include_router(users.router)
-app.include_router(guacamole.router)
 app.include_router(admin.router)
+app.include_router(proxmox.router)
+app.include_router(guacamole.router)
+app.include_router(server.router)
