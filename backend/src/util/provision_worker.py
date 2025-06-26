1a5a1979c294e0a81443c063c7fe69d652d03c0a
Emil S <41843174+OneHandedWonder@users.noreply.github.com>
2025-06-26 12:26:52 +0200

    Increase sleep duration to 180 seconds to allow locks to clear during VM cloning

--- backend/src/util/provision_worker.py
+++ backend/src/util/provision_worker.py
@@ -27,7 +27,7 @@ async def provision_worker(req: ProvisionRequest):
     if "error" in clone_result:
         raise HTTPException(400, detail=clone_result["error"])
 
-    await asyncio.sleep(120)  # allow locks to clear
+    await asyncio.sleep(180) #3 minutes   # allow locks to clear
 
 
     provision_result = proxmox_service.provision_cloud_init_vm_with_retry(template_node, vmid, req)
